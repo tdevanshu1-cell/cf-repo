bddb70ff0df47356ff92ea9b68cbb41fdd62e84f
tdevanshu1-cell <tdevanshu1@gmail.com>
2026-07-06 22:37:22 +0530

    Sync Codeforces: 467A - George and Accommodation [C++17 (GCC 7-32)]

---
+++ 467A_George_and_Accommodation.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,p,q,a=0;
+    cin>>n;
+    while(n--){
+        cin>>p>>q;
+        if(abs(p-q)>=2){
+            a++;
+        }
+ 
+    }
+    cout<<a;
+    return 0;
+}
