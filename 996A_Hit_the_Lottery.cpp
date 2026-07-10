f007ddc945d9ad4bcd5e27a51b06093c8b4b1292
Devanshu Trivedi <tdevanshu1@gmail.com>
2026-07-10 20:57:26 +0530

    Sync Codeforces: 996A - Hit the Lottery [C++17 (GCC 7-32)]

---
+++ 996A_Hit_the_Lottery.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int n,a=0;
+    cin >>n;
+    if(n>=100){
+        a+=n/100;
+        n=n%100;
+       
+    }
+    if(n>=20&&n<100){
+        a+=n/20;
+        n=n%20;
+   
+    }
+    if(n>=10&&n<20){
+        a+=n/10;
+        n=n%10;
+        
+    }
+    if(n>=5&&n<10){
+        a+=n/5;
+        n=n%5;
+        
+    }
+    if(n>=1&&n<5){
+        a+=n/1;
+        n=n%1;
+        
+    }
+    cout<<a;
+ 
+    return 0;
+}
