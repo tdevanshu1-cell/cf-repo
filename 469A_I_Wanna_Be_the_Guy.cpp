9330b38bdf613f545d2eb6245646a42e91101400
Devanshu Trivedi <tdevanshu1@gmail.com>
2026-07-10 20:57:17 +0530

    Sync Codeforces: 469A - I Wanna Be the Guy [C++17 (GCC 7-32)]

--- 469A_I_Wanna_Be_the_Guy.cpp
+++ 469A_I_Wanna_Be_the_Guy.cpp
@@ -9,21 +9,21 @@ int main() {
     
     set<int> unique_levels;
     
-    // Read Little X's levels
+    
     cin >> p;
     for (int i = 0; i < p; i++) {
         cin >> level;
         unique_levels.insert(level);
     }
     
-    // Read Little Y's levels
+    
     cin >> q;
     for (int i = 0; i < q; i++) {
         cin >> level;
         unique_levels.insert(level);
     }
     
-    // Check if they can clear all n levels
+    
     if (unique_levels.size() == n) {
         cout << "I become the guy." << endl;
     } else {
