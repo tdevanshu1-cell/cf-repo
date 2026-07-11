f15c09abf4363b5562e86143357248f857097492
Devanshu Trivedi <tdevanshu1@gmail.com>
2026-07-11 23:05:26 +0530

    Sync Codeforces: 443A - Anton and Letters [C++17 (GCC 7-32)]

---
+++ 443A_Anton_and_Letters.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+ 
+    string s;
+    getline(cin,s);
+    set<char>a;
+   for(int i=0;i<s.size();i++){
+    if(s[i]>='a'&&s[i]<='z'){
+        a.insert(s[i]);
+    }
+   }
+   cout<<a.size();
+ 
+    return 0;
+}
