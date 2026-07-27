09e7080bf267cc2ff5bfdbfb99ca06b08b5517ca
Devanshu Trivedi <tdevanshu1@gmail.com>
2026-07-27 16:53:58 +0530

    Sync Codeforces: 1807A - Plus or Minus [C++17 (GCC 7-32)]

---
+++ 1807A_Plus_or_Minus.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a + b == c)
+        {
+            cout << "+\n";
+        }
+        if (a - b == c)
+        {
+            cout << "-\n";
+        }
+    }
+    return 0;
+}
