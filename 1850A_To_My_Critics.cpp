545214b2d286e3bef2e60d75fcd70c19e5ff9a75
Devanshu Trivedi <tdevanshu1@gmail.com>
2026-08-08 20:43:01 +0530

    Sync Codeforces: 1850A - To My Critics [C++17 (GCC 7-32)]

---
+++ 1850A_To_My_Critics.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a + b >= 10)
+        {
+            cout << "YES\n";
+        }
+        else if (b + c >= 10)
+        {
+            cout << "YES\n";
+        }
+        else if (c + a >= 10)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
