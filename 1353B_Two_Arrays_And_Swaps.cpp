a0c9c5282ec708f4be6a2ba751c28226b3bff155
Devanshu Trivedi <tdevanshu1@gmail.com>
2026-09-18 20:49:23 +0530

    Sync Codeforces: 1353B - Two Arrays And Swaps [C++17 (GCC 7-32)]

---
+++ 1353B_Two_Arrays_And_Swaps.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        long sum = 0;
+        cin >> n >> k;
+        vector<int> a(n);
+        vector<int> b(n);
+        for (int &c : a)
+        {
+            cin >> c;
+        }
+        for (int &c : b)
+        {
+            cin >> c;
+        }
+        while (k--)
+        {
+            sort(a.begin(), a.end());
+            sort(b.begin(), b.end());
+            if (b[n - 1] >= a[0])
+            {
+                a[0] = b[n - 1];
+                b[n - 1] = 0;
+            }
+            else
+            {
+                break;
+            }
+        }
+        while (n--)
+        {
+            sum += a[n];
+        }
+        cout << sum << "\n";
+    }
+    return 0;
+}
